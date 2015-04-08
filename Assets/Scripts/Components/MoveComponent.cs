99fd9582f2da35453da98490699709d7adfc4c15
Awpteamoose <awpteamoose@gmail.com>
2015-04-08 23:39:13 +0300

    both characters are now path, trying to establish camera and playing field

--- Assets/Scripts/Components/MoveComponent.cs
+++ Assets/Scripts/Components/MoveComponent.cs
@@ -6,44 +6,44 @@ public class MoveComponent: MonoBehaviour
 	public float speed;
 	public float rotation_factor;
 
-    public float mod_speed = 1f;
+	public float mod_speed = 1f;
 
-    public Direction direction;
-    public PlayerControl owner { get; set; }
+	public Direction direction;
+	public PlayerControl owner { get; set; }
 
 	void Awake()
-    {
-        direction.angle = GetComponent<Rigidbody2D>().rotation;
-        owner = GetComponent<PlayerControl>();
+	{
+		direction.angle = GetComponent<Rigidbody2D>().rotation;
+		owner = GetComponent<PlayerControl>();
 	}
 
-    void FixedUpdate()
-    {
-        /*
-         * owner.rigidbody.rotation - текущий угол персонажа
-         * direction.angle - угол, в который я хочу повернуться
-         * difference - правый угол вокруг начала оси координат между angle и direction.angle. Правый в смысле, что угол между 45 и -45 будет -90 т.к. кратчайшим путём будет поворот по часовой стрелке (потому минус) на 90 градусов.
-         * Mathf.Rad2Deg и Mathf.Deg2Rad - это константы, которые нужны чтоб превращать углы из градусов в радианы и наоборот. Например, 90 * Mathf.Deg2Rad == 1.57...
-         * Mathf.Atan2(y, x) - арктангенс, тригорометрическая функция, которая по факту возвращет угол между осью Х и вектором (x, y) - http://i.stack.imgur.com/xQiWG.png
-         * Mathf.Sin в данном контексте возвращает мне Y - компоненту моего вектора разворота.
-         * Mathf.Cos делает тоже самое, но для X - компоненты.
-         * Если взять единичный круг (http://www.geocities.ws/petrinamaher/UnitCircle.GIF), то поворот с 0 до 90 градусов - это переход с вектора (cos(0), sin(0)) => (1, 0) на вектор (sin(90), cos(90)) => (0, 1).
-         * Моя задача заключается в том, чтоб узнать в какую сторону поворачиваться короче всего, для этого мне понадобился арк-тангенс.
-         */
-        float difference =
-            Mathf.Rad2Deg * Mathf.Atan2(
-                Mathf.Sin(Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * owner.rigidbody.rotation),
-                Mathf.Cos(Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * owner.rigidbody.rotation)
-            );
-        owner.rigidbody.angularVelocity = difference * rotation_factor;
+	void FixedUpdate()
+	{
+		/*
+		 * owner.rigidbody.rotation - текущий угол персонажа
+		 * direction.angle - угол, в который я хочу повернуться
+		 * difference - правый угол вокруг начала оси координат между angle и direction.angle. Правый в смысле, что угол между 45 и -45 будет -90 т.к. кратчайшим путём будет поворот по часовой стрелке (потому минус) на 90 градусов.
+		 * Mathf.Rad2Deg и Mathf.Deg2Rad - это константы, которые нужны чтоб превращать углы из градусов в радианы и наоборот. Например, 90 * Mathf.Deg2Rad == 1.57...
+		 * Mathf.Atan2(y, x) - арктангенс, тригорометрическая функция, которая по факту возвращет угол между осью Х и вектором (x, y) - http://i.stack.imgur.com/xQiWG.png
+		 * Mathf.Sin в данном контексте возвращает мне Y - компоненту моего вектора разворота.
+		 * Mathf.Cos делает тоже самое, но для X - компоненты.
+		 * Если взять единичный круг (http://www.geocities.ws/petrinamaher/UnitCircle.GIF), то поворот с 0 до 90 градусов - это переход с вектора (cos(0), sin(0)) => (1, 0) на вектор (sin(90), cos(90)) => (0, 1).
+		 * Моя задача заключается в том, чтоб узнать в какую сторону поворачиваться короче всего, для этого мне понадобился арк-тангенс.
+		 */
+		float difference =
+			Mathf.Rad2Deg * Mathf.Atan2(
+				Mathf.Sin(Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * owner.rigidbody.rotation),
+				Mathf.Cos(Mathf.Deg2Rad * direction.angle - Mathf.Deg2Rad * owner.rigidbody.rotation)
+			);
+		owner.rigidbody.angularVelocity = difference * rotation_factor;
 
-        if (owner.rigidbody.velocity.magnitude > 1f)
-        {
-            owner.animator.SetBool("Moving", true);
-        }
-        else
-        {
-            owner.animator.SetBool("Moving", false);
-        }
-    }
+		if (owner.rigidbody.velocity.magnitude > 1f)
+		{
+			//owner.animator.SetBool("Moving", true);
+		}
+		else
+		{
+			//owner.animator.SetBool("Moving", false);
+		}
+	}
 }
--- Assets/Scripts/Components/Player/Components/PlayerControl.cs
+++ Assets/Scripts/Components/Player/Components/PlayerControl.cs
@@ -9,18 +9,18 @@ public class PlayerControl: MonoBehaviour
 	public CastComponent castComponent {get; set;}
 	public PlayerHealthComponent healthComponent {get; set;}
 	public PlayerInputComponent inputComponent {get; set;}
-    public Animator animator { get; set; }
-    public SpriteRenderer spriteRenderer { get; set; }
-    public new Transform transform { get; set; }
-    public new Rigidbody2D rigidbody { get; set; }
+	public Animator animator { get; set; }
+	public SpriteRenderer spriteRenderer { get; set; }
+	public new Transform transform { get; set; }
+	public new Rigidbody2D rigidbody { get; set; }
 
-    public GameObject arrow;
-    public UltimateSpell ultimate;
+	public GameObject arrow;
+	public UltimateSpell ultimate;
 	public string player = "Player 1";
-    public AudioClip[] words;
-    public AudioClip[] ultimateSounds;
+	public AudioClip[] words;
+	public AudioClip[] ultimateSounds;
 
-    public static List<PlayerControl> activePlayers;
+	public static List<PlayerControl> activePlayers;
 	
 	public StateMachine.Machine<States> sm = new StateMachine.Machine<States> ();
 	public enum States
@@ -29,46 +29,46 @@ public class PlayerControl: MonoBehaviour
 		Cast
 	}
 
-    void Awake()
-    {
-        if (PlayerControl.activePlayers == null)
-            PlayerControl.activePlayers = new List<PlayerControl>();
-        PlayerControl.activePlayers.Add(this);
+	void Awake()
+	{
+		if (PlayerControl.activePlayers == null)
+			PlayerControl.activePlayers = new List<PlayerControl>();
+		PlayerControl.activePlayers.Add(this);
 
-        moveComponent = GetComponent<MoveComponent>();
-        castComponent = GetComponent<CastComponent>();
-        healthComponent = GetComponent<PlayerHealthComponent>();
-        inputComponent = GetComponent<PlayerInputComponent>();
+		moveComponent = GetComponent<MoveComponent>();
+		castComponent = GetComponent<CastComponent>();
+		healthComponent = GetComponent<PlayerHealthComponent>();
+		inputComponent = GetComponent<PlayerInputComponent>();
 
-        animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        transform = GetComponent<Transform>();
-        rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		transform = GetComponent<Transform>();
+		rigidbody = GetComponent<Rigidbody2D>();
 
-        inputComponent.playername = player;
-    }
+		inputComponent.playername = player;
+	}
 
 	// Use this for initialization
 	void Start ()
-    {
+	{
 		for (int i = 0; i<4; i++)
 		{
-            //PlayerPrefs.DeleteAll();
-            string spellName = player + " SelectedSpell " + i.ToString();
-            int spellNumber = PlayerPrefs.GetInt(spellName, -1);
-            if (spellNumber == -1 || spellNumber >= SpellList.Count)
-            {
-                spellNumber = Random.Range(0, SpellList.Count);
-                PlayerPrefs.SetInt(spellName, spellNumber);
-            }
-            castComponent.spellBook.Set(spellNumber, this, i);
+			//PlayerPrefs.DeleteAll();
+			string spellName = player + " SelectedSpell " + i.ToString();
+			int spellNumber = PlayerPrefs.GetInt(spellName, -1);
+			if (spellNumber == -1 || spellNumber >= SpellList.Count)
+			{
+				spellNumber = Random.Range(0, SpellList.Count);
+				PlayerPrefs.SetInt(spellName, spellNumber);
+			}
+			castComponent.spellBook.Set(spellNumber, this, i);
 		}
-        castComponent.altAimMode = (PlayerPrefs.GetInt(player + " Aim Mode", 0) == 0)?false:true;
-        castComponent.reticle_speed = PlayerPrefs.GetFloat(player + " Reticle Speed", 3f);
-        castComponent.spellBook.Set(ScriptableObject.Instantiate(ultimate) as Spell, this, 4);
-        castComponent.spellBook.Set(ScriptableObject.Instantiate(SpellList.normalAttack) as Spell, this, 5);
-        castComponent.spellBook.ActivateAll();
-        PlayerPrefs.Save();
+		castComponent.altAimMode = (PlayerPrefs.GetInt(player + " Aim Mode", 0) == 0)?false:true;
+		castComponent.reticle_speed = PlayerPrefs.GetFloat(player + " Reticle Speed", 3f);
+		castComponent.spellBook.Set(ScriptableObject.Instantiate(ultimate) as Spell, this, 4);
+		castComponent.spellBook.Set(ScriptableObject.Instantiate(SpellList.normalAttack) as Spell, this, 5);
+		castComponent.spellBook.ActivateAll();
+		PlayerPrefs.Save();
 
 		sm.states.Add(States.Move,new PlayerMoveState(this));
 		sm.states.Add(States.Cast,new PlayerCastState(this));
@@ -78,36 +78,36 @@ public class PlayerControl: MonoBehaviour
 	
 	void Update()
 	{
-        if (!PauseMenu.paused)
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                if (Input.GetButtonDown(player + " Switch Spell " + (i + 1)))
-                {
-                    string spellName = player + " SelectedSpell " + i.ToString();
-                    int newSpell = PlayerPrefs.GetInt(spellName) + 1;
-                    if (newSpell >= SpellList.Count)
-                        newSpell = 0;
-                    PlayerPrefs.SetInt(spellName, newSpell);
-                    castComponent.spellBook.Set(newSpell, this, i);
-                    castComponent.spellBook.Activate(i);
-                    PlayerPrefs.Save();
-                }
-            }
+		if (!PauseMenu.paused)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				if (Input.GetButtonDown(player + " Switch Spell " + (i + 1)))
+				{
+					string spellName = player + " SelectedSpell " + i.ToString();
+					int newSpell = PlayerPrefs.GetInt(spellName) + 1;
+					if (newSpell >= SpellList.Count)
+						newSpell = 0;
+					PlayerPrefs.SetInt(spellName, newSpell);
+					castComponent.spellBook.Set(newSpell, this, i);
+					castComponent.spellBook.Activate(i);
+					PlayerPrefs.Save();
+				}
+			}
 
-            sm.Update();
-        }
+			sm.Update();
+		}
 	}
 
-    void OnBecameInvisible()
-    {
-        arrow.SetActive(true);
-    }
+	void OnBecameInvisible()
+	{
+		arrow.SetActive(true);
+	}
 
-    void OnBecameVisible()
-    {
-        arrow.SetActive(false);
-    }
+	void OnBecameVisible()
+	{
+		arrow.SetActive(false);
+	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
